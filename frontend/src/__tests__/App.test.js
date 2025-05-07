5c237d28bd5acd7aa7ef346610e0ff88263cd9fd
ibkam <ibrahimkamau254@gmail>
2025-05-07 14:52:31 +0300

    update_test_library_jest-dom

--- frontend/src/__tests__/App.test.js
+++ frontend/src/__tests__/App.test.js
@@ -1,9 +1,10 @@
+import '@testing-library/jest-dom';
+import React from "react";
 import { render, screen } from '@testing-library/react';
-import App from '../index.css';
+import App from '../App';
 
 test('renders hello from react text', () => {
   render(<App />);
   const linkElement = screen.getByText(/hello from react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
